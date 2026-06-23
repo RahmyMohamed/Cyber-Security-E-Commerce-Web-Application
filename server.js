388cb8220ed1488e721ff3892f4e6bed7b0e833a
RahmyMohamed <mohamedrahmi428@gmail.com>
2026-06-23 13:52:55 +0530

    hosting update

--- server.js
+++ server.js
@@ -7,7 +7,9 @@ const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
 
 const app = express();
+const path = require('path');
 
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
